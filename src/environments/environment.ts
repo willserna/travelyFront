54d0438aab4e846aec8b9d393da32f39df8bbff7
William Serna <williamsernac@hotmail.com>
2023-04-12 18:22:30 -0500

    extra setup

---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  firebase: {
+    projectId: 'travely-63cc3',
+    appId: '1:283556776644:web:efc1770b816ca16d395791',
+    storageBucket: 'travely-63cc3.appspot.com',
+    apiKey: 'AIzaSyDEG14syJvQGJ7C-YGj5EODBUJViKLXiL0',
+    authDomain: 'travely-63cc3.firebaseapp.com',
+    messagingSenderId: '283556776644',
+  },};

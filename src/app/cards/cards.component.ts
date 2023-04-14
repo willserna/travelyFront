37adb86a98e2955b6e921b24d0ae6c908e3d5fe9
William Serna <williamsernac@hotmail.com>
2023-04-14 14:15:55 -0500

    Card component modified

--- src/app/cards/cards.component.ts
+++ src/app/cards/cards.component.ts
@@ -1,34 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductService } from 'src/services/product.service';
+import { Product } from '../models/product.model';
 
-interface Destination {
-  title: string;
-  description: string;
-  imageSrc: string;
-}
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit {
 
-  featuredDestinations: Destination[] = [
-    {
-      title: 'Paris, France',
-      description: 'Experience the romance of the City of Love with its iconic landmarks, exquisite cuisine and cultural charm.',
-      imageSrc: '../../assets/images/paris.jpeg'
-    },
-    {
-      title: 'Tokyo, Japan',
-      description: 'Immerse yourself in the bustling metropolis of Tokyo with its unique blend of traditional and modern culture.',
-      imageSrc: '../../assets/images/tokyo.jpeg'
-    },
-    {
-      title: 'Sydney, Australia',
-      description: 'Discover the beauty of the Harbour City with its stunning beaches, vibrant arts scene, and iconic landmarks.',
-      imageSrc: '../../assets/images/sydney.jpeg'
-    }
-  ];
+  constructor(private router: Router, private service: ProductService){}
+  products_found: Product[] = [];
+
+  ngOnInit(): void {
+
+    this.service.getAll().subscribe({
+      next: (product) => {
+        this.products_found = product
+      }
+    })
+
+  }
+
+
+
+
+
+
+
+//   featuredDestinations: Destination[] = [
+//     {
+//       title: 'Paris, France',
+//       description: 'Experience the romance of the City of Love with its iconic landmarks, exquisite cuisine and cultural charm.',
+//       imageSrc: '../../assets/images/paris.jpeg'
+//     },
+//     {
+//       title: 'Tokyo, Japan',
+//       description: 'Immerse yourself in the bustling metropolis of Tokyo with its unique blend of traditional and modern culture.',
+//       imageSrc: '../../assets/images/tokyo.jpeg'
+//     },
+//     {
+//       title: 'Sydney, Australia',
+//       description: 'Discover the beauty of the Harbour City with its stunning beaches, vibrant arts scene, and iconic landmarks.',
+//       imageSrc: '../../assets/images/sydney.jpeg'
+//     },
+//     {
+//       title: 'Paris, France',
+//       description: 'Experience the romance of the City of Love with its iconic landmarks, exquisite cuisine and cultural charm.',
+//       imageSrc: '../../assets/images/paris.jpeg'
+//     },
+//     {
+//       title: 'Tokyo, Japan',
+//       description: 'Immerse yourself in the bustling metropolis of Tokyo with its unique blend of traditional and modern culture.',
+//       imageSrc: '../../assets/images/tokyo.jpeg'
+//     },
+//     {
+//       title: 'Sydney, Australia',
+//       description: 'Discover the beauty of the Harbour City with its stunning beaches, vibrant arts scene, and iconic landmarks.',
+//       imageSrc: '../../assets/images/sydney.jpeg'
+//     },
+//     {
+//       title: 'Paris, France',
+//       description: 'Experience the romance of the City of Love with its iconic landmarks, exquisite cuisine and cultural charm.',
+//       imageSrc: '../../assets/images/paris.jpeg'
+//     },
+//     {
+//       title: 'Tokyo, Japan',
+//       description: 'Immerse yourself in the bustling metropolis of Tokyo with its unique blend of traditional and modern culture.',
+//       imageSrc: '../../assets/images/tokyo.jpeg'
+//     },
+//     {
+//       title: 'Sydney, Australia',
+//       description: 'Discover the beauty of the Harbour City with its stunning beaches, vibrant arts scene, and iconic landmarks.',
+//       imageSrc: '../../assets/images/sydney.jpeg'
+//     }
+//   ];
 
 }

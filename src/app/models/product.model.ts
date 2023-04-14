f2590dfea6357ee1a6026571f4ca6f79cbc5b6b4
William Serna <williamsernac@hotmail.com>
2023-04-14 14:17:22 -0500

    added product model for API consumption

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,14 @@
+export interface Product {
+  id: string,
+  packageName: string,
+  details: string,
+  location: string,
+  stock: number,
+  startDate: Date,
+  persons: number,
+  featured: boolean,
+  category: string,
+  description: string,
+  image: string,
+  price: number
+}

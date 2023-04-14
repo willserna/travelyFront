3091461549c8c8826626b72c2f3e2714d00cd7c2
William Serna <williamsernac@hotmail.com>
2023-04-14 14:16:32 -0500

    Modified destinations page

--- src/app/destinations/destinations.component.ts
+++ src/app/destinations/destinations.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from 'src/services/product.service';
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-destinations',
@@ -7,15 +9,45 @@ import { Component } from '@angular/core';
 })
 export class DestinationsComponent {
 
+  constructor(private service: ProductService){}
+
   destination: string = '';
   category: string = '';
   date: string = '';
+  products_found: Product[] = [];
+  showDetails: boolean = false;
 
   searchDestinations() {
-    // Implement your search logic here
+
     console.log('Destination:', this.destination);
     console.log('Category:', this.category);
     console.log('Date:', this.date);
+
+    if (this.destination == ''){
+      console.log("No destination entered")
+    }
+    else{
+      console.log("Destination entered")
+      this.service.getByName(this.destination).subscribe({
+        next: (product) => {
+          this.products_found = product
+        }
+      })
+      this.showDetails = true;
+    }
+
+    if (this.category == ''){
+      console.log("No category entered")
+    }
+    else{
+      console.log("Category entered")
+      this.service.getByCategory(this.category).subscribe({
+        next: (product) => {
+          this.products_found = product
+        }
+      })
+      this.showDetails = true;
+    }
   }
 
 }
